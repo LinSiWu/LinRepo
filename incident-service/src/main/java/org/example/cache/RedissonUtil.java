33472937aeb34ec3d4c014c8f359a604998074d7
LinLin <Lin.Lin1@viatris.com>
2024-12-20 20:34:49 +0800

    commit framework redisson

---
+++ incident-service/src/main/java/org/example/cache/RedissonUtil.java
@@ -0,0 +1,57 @@
+package org.example.cache;
+
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.*;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@Slf4j
+public class RedissonUtil {
+
+    @Resource
+    private RedissonClient redissonClient;
+
+
+    // 批量查询 返回K V map
+    public Map<String, Object> getBatchByKeys(List<String> keys) {
+
+        Map<String, Object> resultMap = new HashMap<>();
+        Map<String, RFuture<Object>> rFutureMap = new HashMap<>();
+        RBatch batch = redissonClient.createBatch();
+        for (String key : keys) {
+            RBucketAsync<Object> bucketAsync = batch.getBucket(key);
+            rFutureMap.put(key, bucketAsync.getAsync());
+        }
+        batch.execute();
+
+        for (Map.Entry<String, RFuture<Object>> entry : rFutureMap.entrySet()) {
+            try {
+                resultMap.put(entry.getKey(), entry.getValue().get());
+            } catch (ExecutionException e) {
+                log.error("error get redis key: {} with ExecutionException", entry.getKey(), e);
+            } catch (InterruptedException e) {
+                log.error("error get redis key: {} with InterruptedException", entry.getKey(), e);
+                Thread.currentThread().interrupt();
+            }
+        }
+        return resultMap;
+    }
+
+    // 批量存储 设置统一过期时间
+    public void setBatchByMap(Map<String, Object> map, long timeToLive, TimeUnit timeUnit) {
+
+        RBatch batch = redissonClient.createBatch();
+        for (Map.Entry<String, Object> stringObjectEntry : map.entrySet()) {
+            RBucketAsync<Object> bucketAsync = batch.getBucket(stringObjectEntry.getKey());
+            bucketAsync.setAsync(stringObjectEntry.getValue(), timeToLive, timeUnit);
+        }
+        batch.execute();
+    }
+}
